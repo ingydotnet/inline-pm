3d3ef0f75cc29bea07c4af6949826ae295ab2784
Ed J <mohawk2@users.noreply.github.com>
2014-07-11 07:17:51 +0100

    0.58

--- lib/Inline.pm
+++ lib/Inline.pm
@@ -1,7 +1,7 @@
 use strict; use warnings;
 package Inline;
 
-our $VERSION = '0.57';
+our $VERSION = '0.58';
 
 use Inline::denter;
 use Config;
--- lib/Inline/Foo.pm
+++ lib/Inline/Foo.pm
@@ -3,7 +3,7 @@ package Inline::Foo;
 
 require Inline;
 our @ISA = qw(Inline);
-our $VERSION = '0.57';
+our $VERSION = '0.58';
 
 use Carp;
 use File::Spec;
