07ece06c9df953da248c49b185a616be6e7e566a
Ingy döt Net <ingy@ingy.net>
2014-07-13 21:55:31 -0700

    0.62

--- lib/Inline.pm
+++ lib/Inline.pm
@@ -1,7 +1,7 @@
 use strict; use warnings;
 package Inline;
 
-our $VERSION = '0.61';
+our $VERSION = '0.62';
 
 use Inline::denter;
 use Config;
--- lib/Inline/Foo.pm
+++ lib/Inline/Foo.pm
@@ -3,7 +3,7 @@ package Inline::Foo;
 
 require Inline;
 our @ISA = qw(Inline);
-our $VERSION = '0.61';
+our $VERSION = '0.62';
 
 use Carp;
 use File::Spec;
