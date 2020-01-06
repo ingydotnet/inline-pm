35dc88b814652017d8dd348e637345c2948df931
Ingy döt Net <ingy@ingy.net>
2020-01-05 22:50:00 -0800

    Remove grepping out TestML paths
    The test evidently doesn't need that.

--- test/TestMLBridge.pm
+++ test/TestMLBridge.pm
@@ -3,7 +3,7 @@ package TestMLBridge;
 use base 'TestML::Bridge';
 
 use Inline;
-use File::Spec::Functions qw(rel2abs abs2rel);
+use File::Spec::Functions qw(abs2rel);
 
 sub derive_minus_i {
     my ($self, $perl) = @_;
@@ -11,13 +11,10 @@ sub derive_minus_i {
     local @INC = @INC;
     eval $perl;
 
-    my @paths = map abs2rel($_), grep {
-        $_ ne rel2abs('lib') &&
-        $_ ne rel2abs('test') &&
-        $_ ne rel2abs('../testml/src/perl5/lib')
-    } Inline->derive_minus_I;
-
-    join '', map "$_\n", @paths;
+    join '',
+        map "$_\n",
+        map abs2rel($_),
+        Inline->derive_minus_I;
 }
 
 1;
